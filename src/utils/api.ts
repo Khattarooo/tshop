1e59dc12c6b9854b67124f4f34a151241c996b05
Khattarooo <tony_khattar_2001@live.com>
2024-05-07 13:03:25 +0300

    configuring env

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,9 +1,10 @@
 import axios from 'axios';
+import Config from 'react-native-config';
 
 export const api = axios.create({
-  baseURL: 'https://backend-practice.euriskomobility.me/',
+  baseURL: Config.API_URL,
 });
 
 export const mockApi = axios.create({
-  baseURL: 'https://6628b3a154afcabd07369c31.mockapi.io/',
+  baseURL: Config.MOCK_API_URL,
 });
